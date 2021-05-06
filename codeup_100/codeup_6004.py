a9a3da0219ae4983b42a673bcf02ea61e61b9dee
Seung-ock JUNG <sojung53@gmail.com>
2021-05-06 23:53:03 +0900

    출력하기04

---
+++ codeup_100/codeup_6004.py
@@ -0,0 +1,11 @@
+'''
+작은 따옴표(')(single quotation mark)가 들어있는 출력문
+
+'Hello'
+'''
+
+# 방법1 - 큰 따옴표 안에 작은 따옴표를 입력 (크로스 되지 않도록 주의)
+print("'Hello'")
+
+# 방법2 - .format옵션 사용
+print('{0}'.format("'Hello'"))
