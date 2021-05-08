55876c148eea24abb9fb695f571e480c09b52783
Seung-ock JUNG <sojung53@gmail.com>
2021-05-08 23:08:11 +0900

    정수 2개 입력받아 합 계산하기

---
+++ codeup_100/codeup_6025.py
@@ -0,0 +1,23 @@
+'''
+정수 2개를 입력받아
+합을 출력하기
+
+입력예시
+hello world
+
+출력예시
+helloworld
+
+'''
+
+while(True):
+    data = input('숫자를 2개 입력해주세요. (예) 24 813\n->')
+
+    try:
+        d1, d2 = data.split()
+    except:
+        continue
+    else:
+        break
+d = int(d1) + int(d2)
+print(d)
