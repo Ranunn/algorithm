87febfc7494a6203d8deddcafb6715989fb39f8c
Seung-ock JUNG <sojung53@gmail.com>
2021-05-11 23:39:49 +0900

    정수 2개 입력받아 나눈 나머지 계산하기

---
+++ codeup_100/codeup_6041.py
@@ -0,0 +1,32 @@
+'''
+정수 2개(a, b)를 입력받아 a를 b로 나눈 나머지를 출력하기
+
+-----------------------------------------------------------
+
+입력예시
+10 3
+
+출력예시
+1
+
+'''
+
+while(True):
+    ints = input('나누기할 정수를 입력해주세요. (예) 10 3\n->')
+    floats = input('나누기할 실수를 입력해주세요. (예) 10.0 3.0\n->')
+
+    try:
+        a, b = ints.split()
+        a = int(a)
+        b = int(b)
+        f1, f2 = floats.split()
+        f1 = float(f1)
+        f2 = float(f2)
+    except:
+        print('잘못 입력하였습니다.')
+        continue
+    else:
+        break
+
+print(a % b)   # 정수로 떨어지는 몫만 출력
+print(f1 % f2) # 실수로 떨어지는 몫만 출력
